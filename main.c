df70887dec57c4d7c71b6c953818713830e15d80
Berat Yılmaz <adnanberatyilmaz@gmail.com>
2022-12-10 17:25:10 +0300

    init image double array with random values

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GRAYSCALE (65536 / 4) - 1
+
 int main(int argc, char* argv[])
 {
     int rowImg, colImg;
@@ -13,13 +15,32 @@ int main(int argc, char* argv[])
     rowImg = atoi(argv[1]);
     colImg = atoi(argv[2]);
 
-    int** image = (int**)malloc(rowImg * sizeof(int*));
+    unsigned short** image = (unsigned short**)malloc(rowImg * sizeof(unsigned short*));
+    unsigned short** resultImage = (unsigned short**)malloc(rowImg * sizeof(unsigned short*));
 
     for(int i = 0; i < rowImg; i++)
     {
-        image[i] = (int*)malloc(colImg * sizeof(int));
+        image[i] = (unsigned short*)malloc(colImg * sizeof(unsigned short));
+        resultImage[i] = (unsigned short*)malloc(colImg * sizeof(unsigned short));
     }
 
+    for(int i = 0; i < rowImg; i++)
+    {
+        for(int j = 0; j < colImg; j++)
+        {
+            image[i][j] = rand() % GRAYSCALE;
+        }
+    }
+
+    // Print Original Image
+    for(int i = 0; i < rowImg; i++)
+    {
+        for(int j = 0; j < colImg; j++)
+        {
+            printf("%d ", image[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
