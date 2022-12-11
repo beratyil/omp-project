d0e79ad391e7f07527c6554051641151b238f454
Berat Yılmaz <adnanberatyilmaz@gmail.com>
2022-12-11 09:20:16 +0300

    multiply A and B matrix
    Matrices A and B are created and read from original image. Then C matrix is created and the result of A multiply B is assigned it to C.

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define GRAYSCALE (65536 / 4) - 1
+
+void matrixMultiplication(unsigned short matrix1[5][5], unsigned short matrix2[5][5], unsigned short** result);
 
 int main(int argc, char* argv[])
 {
@@ -33,7 +36,7 @@ int main(int argc, char* argv[])
     }
     
     /* Print Original Image */
-    //
+
     for(int i = 0; i < rowImg; i++)
     {
         for(int j = 0; j < colImg; j++)
@@ -45,26 +48,45 @@ int main(int argc, char* argv[])
 
     for(int imgRowIndx = 0; imgRowIndx < rowImg; imgRowIndx++)
     {
-        for(int imgColIndx = 0; imgColIndx < rowImg; imgColIndx++)
+        for(int imgColIndx = 0; imgColIndx <= imgRowIndx; imgColIndx++)
         {
             
-            unsigned int A[5][5];
-            unsigned int B[5][5];
+            unsigned short A[5][5];
+            unsigned short B[5][5];
 
-            for(int kernelRowIndx = imgRowIndx; kernelRowIndx < imgRowIndx + 5; kernelRowIndx++)
+            /* Prepare A and B matrices */
+            for(int kernelRowIndx = 0; kernelRowIndx < 5; kernelRowIndx++)
             {
-                for(int kernelColIndx = imgColIndx; kernelColIndx < imgRowIndx + 5; kernelColIndx++)
+                for(int kernelColIndx = 0; kernelColIndx < 5; kernelColIndx++)
                 {
-                    
-
+                    A[kernelRowIndx][kernelColIndx] = image[imgRowIndx][imgColIndx];
+                    B[kernelRowIndx][kernelColIndx] = image[imgColIndx][imgRowIndx];
                 }
-
-
             }
+
+            unsigned short C[5][5];
+            memset(C, 0, sizeof(C));
+
+            matrixMultiplication(A, B, &*C);
 
         }
 
     }
 
     return 0;
+}
+
+void matrixMultiplication(unsigned short matrix1[5][5], unsigned short matrix2[5][5], unsigned short** result)
+{
+    for(int row = 0; row < 5; row++)
+    {
+        for(int col = 0; col < 5; col++)
+        {
+            for(int colRow = 0; colRow < 5; colRow++)
+            {
+                result[row][col] += matrix1[row][colRow] * matrix2[colRow][col];
+            }
+        }
+    }
+    return;    
 }
